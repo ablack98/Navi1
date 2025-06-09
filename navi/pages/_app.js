4daaf4bedba03b5dd66dabea5b21282c7f7f4bd8
ablack98 <lespaul98@hotmail.com>
2025-06-09 12:41:43 +1000

    Update _app.js

--- navi/pages/_app.js
+++ navi/pages/_app.js
@@ -26,3 +26,23 @@ export default function App({ Component, pageProps }) {
     </div>
   );
 }
+
+// pages/_app.js
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div style={{
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0
+    }}>
+      <div className="page-overlay">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
+}
+
