e8ec8c1ab098cc7c1dbe62ab9f7dbdde30eeb2af
ablack98 <lespaul98@hotmail.com>
2025-06-09 12:53:11 +1000

    Update _app.js

--- navi/pages/_app.js
+++ navi/pages/_app.js
@@ -1,11 +1,18 @@
-// File: pages/_app.js
-
-import '../styles/globals.css';  // Make sure this import is at the very top
+// pages/_app.js
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    // Full‐screen background wrapper
-    <div
-      style={{
-        position: 'fixed',
-        width
+    <div style={{
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+    }}>
+      <div className="page-overlay">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
+}
